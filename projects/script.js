36b5fe0fabf8f9d03cb5d07bca968b6dc7c2fb92
annu12340 <annujolly17@gmail.com>
2019-08-09 23:28:04 +0530

    added files

---
+++ projects/script.js
@@ -0,0 +1,27 @@
+
+
+jQuery(document).ready(function($) {
+            "use strict";
+            //  TESTIMONIALS CAROUSEL HOOK
+            $('#customers-testimonials').owlCarousel({
+                loop: true,
+                center: true,
+                items: 5,
+                margin: 0,
+                autoplay: true,
+                dots:true,
+                autoplayTimeout: 4500,
+                smartSpeed: 400,
+                responsive: {
+                  0: {
+                    items: 1
+                  },
+                  768: {
+                    items: 2
+                  },
+                  1170: {
+                    items: 3
+                  }
+                }
+            });
+          });
